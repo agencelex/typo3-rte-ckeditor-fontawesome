62abb9cbce57d5283bba984b0c62d892dcb1cdd4
Philippe <philippe@agencelex.com>
2026-05-09 12:11:17 -0400

    Make the extension compatible with TYPO3 14 and refactored it.

--- ext_localconf.php
+++ ext_localconf.php
@@ -6,7 +6,6 @@ defined('TYPO3') or die();
 
 (function () {
     // add TS Config
-    ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:rte_ckeditor_fontawesome/Configuration/TSConfig/rte.txt">');
     // Fontawesome V6
     $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['default'] = 'EXT:rte_ckeditor_fontawesome/Configuration/RTE/DefaultFA6.yaml';
     $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['full'] = 'EXT:rte_ckeditor_fontawesome/Configuration/RTE/FullFA6.yaml';
